ea5a496de14ec248f09ce500de23227c0d80b26f
Ameer77-tech <ameershaik.cs@gmail.com>
2025-12-12 20:54:22 +0530

    feat: side bar for mobile

--- client/components/Home/Header.tsx
+++ client/components/Home/Header.tsx
@@ -4,9 +4,14 @@ import Link from 'next/link';
 import { manrope } from '@/fonts/Fonts';
 import { Button } from '../ui/button';
 import { ArrowRightCircleIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import SideBar from './SideBar';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
   return (
+    <>
+    <SideBar isOpen={isOpen} setIsOpen={setIsOpen}/>
     <header className="flex items-center justify-between px-8 py-4 bg-background/80 backdrop-blur-md sticky top-0 z-50">
       <div className="flex items-center">
         <Image alt="logo" src="/logo.png" width={40} height={50} />
@@ -26,11 +31,23 @@ const Header = () => {
             Templates
         </Link>
           </div>
+          <div className='flex gap-2 items-center'>
         <Button className="flex items-center gap-2">
           Try It <ArrowRightCircleIcon className="w-5 h-5" />
         </Button>
+        <div className='lg:hidden md:hidden'>
+          { isOpen ?  <p onClick={()=>setIsOpen(false)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg></p>
+:   <p onClick={()=>setIsOpen(true)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+        </svg></p> }
+      
+          </div>
+        </div>
     
     </header>
+    </>
   );
 };
 
