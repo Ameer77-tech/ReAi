61cb14232a6cf20484a8835c1abc40d89aa6f735
Ameer77-tech <ameershaik.cs@gmail.com>
2025-12-15 19:10:02 +0530

    animated border around form

--- client/app/(main)/resume/builder/page.tsx
+++ client/app/(main)/resume/builder/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { useResumeStore } from "@/app/store/store";
+import GlowBorder from "@/components/Builder/BorderGlow";
 import ProgressBar from "@/components/Builder/Progress";
 import { Button } from "@/components/ui/button";
 
-const page = () => {
+const Page = () => {
   const step = useResumeStore((state) => state.step);
   const setStep = useResumeStore((state) => state.setStep);
   return (
     <div className="min-h-screen w-full flex lg:flex-row flex-col items-center justify-evenly">
-      <ProgressBar />
-      <Button onClick={() => step < 7 && setStep(step + 1)}>Next Step</Button>
-      <Button onClick={() => step > 1 && setStep(step - 1)}>prev Step</Button>
+      <GlowBorder>
+        <div className="lg:w-full w-full h-full lg:h-full rounded-2xl backdrop-blur-xs bg-background flex lg:flex-row flex-col justify-evenly items-center">
+          <ProgressBar />
+        </div>
+      </GlowBorder>
     </div>
   );
 };
 
-export default page;
+export default Page;
---
+++ client/components/Builder/BorderGlow.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function GlowBorder({ children }: Props) {
+  return (
+    <div className="relative rounded-xl p-0.5 overflow-hidden lg:w-3/4 w-full h-180 lg:h-150">
+      {/* animated glow */}
+      <motion.div
+        className="absolute inset-0 blur-md opacity-80"
+        style={{
+          backgroundImage:
+            "conic-gradient(from 0deg, #f97316, #fdba74, #f97316)",
+        }}
+        animate={{ rotate: 360 }}
+        transition={{
+          duration: 8,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+      />
+
+      {/* content */}
+      {children}
+    </div>
+  );
+}
