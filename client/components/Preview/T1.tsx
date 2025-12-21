97352eabcc35a3a9bc7f527905cbcb623bfff37c
Ameer77-tech <ameershaik.cs@gmail.com>
2025-12-21 20:31:05 +0530

    feat: About Section t1

--- client/components/Preview/T1.tsx
+++ client/components/Preview/T1.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Card, CardContent, CardFooter, CardTitle } from "../ui/card";
+
 import { manrope } from "@/fonts/Fonts";
 import { cn } from "@/lib/utils";
-import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
-import { LocateIcon, LocationEdit } from "lucide-react";
+import Header from "./components/t1/Header";
+import About from "./components/t1/About";
 
 const T1 = () => {
   return (
@@ -14,27 +14,8 @@ const T1 = () => {
         "antialiased"
       )}
     >
-      <Card className="bg-transparent rounded-none border-0 shadow-none border-b-2 border-black place-items-center gap-2">
-        <CardTitle className="text-5xl text-black font-black tracking-wide">
-          AMEER SHAIK
-        </CardTitle>
-        <CardContent className="capitalize text-xl text-black/70 font-bold tracking-wide">
-          Full Stack Developer
-        </CardContent>
-        <CardFooter className="text-black/60 text-sm justify-between w-full">
-          <div className="flex gap-1 items-center mt-2">
-            <PhoneIcon className="size-4 text-black" /> +91 9912437786{" "}
-          </div>
-          <div className="flex gap-1 items-center mt-2">
-            <EnvelopeIcon className="size-4 text-black" />{" "}
-            ameershaik.cs@gmail.com{" "}
-          </div>
-          <div className="flex gap-1 items-center mt-2">
-            <LocationEdit className="size-4 text-black" /> Kakinada,
-            AndhraPradesh{" "}
-          </div>
-        </CardFooter>
-      </Card>
+      <Header />
+      <About />
     </div>
   );
 };
---
+++ client/components/Preview/components/t1/About.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const About = () => {
+  return (
+    <Card className="bg-transparent rounded-none border-0 shadow-none border-b-2 border-black gap-2">
+      <CardTitle className="text-black font-extrabold text-2xl uppercase tracking-wide">
+        About Me
+      </CardTitle>
+      <CardContent className="text-black/70">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam fuga
+        voluptate quia eveniet iure necessitatibus ipsam totam rem dolorum, quae
+        cum aperiam et laborum deserunt dolor vitae saepe esse beatae!voluptate
+        quia eveniet iure necessitatibus ipsam totam rem dolorum, quae cum
+        aperiam et laborum deserunt dolor vitae saepe esse beatae!
+      </CardContent>
+    </Card>
+  );
+};
+
+export default About;
