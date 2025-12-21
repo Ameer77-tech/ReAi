b8f7abf520d81e2a3f8e67be6ad71a51e2a8f4bd
Ameer77-tech <ameershaik.cs@gmail.com>
2025-12-21 20:25:53 +0530

    feat: template 1 header complete

--- client/components/Preview/T1.tsx
+++ client/components/Preview/T1.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { Card, CardContent, CardFooter, CardTitle } from "../ui/card";
+import { manrope } from "@/fonts/Fonts";
+import { cn } from "@/lib/utils";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import { LocateIcon, LocationEdit } from "lucide-react";
 
 const T1 = () => {
   return (
-    <div className="bg-white w-[800px] aspect-[1/1.414]"></div>
+    <div
+      className={cn(
+        `bg-white w-[800px] aspect-[1/1.414] p-10`,
+        manrope.className,
+        "antialiased"
+      )}
+    >
+      <Card className="bg-transparent rounded-none border-0 shadow-none border-b-2 border-black place-items-center gap-2">
+        <CardTitle className="text-5xl text-black font-black tracking-wide">
+          AMEER SHAIK
+        </CardTitle>
+        <CardContent className="capitalize text-xl text-black/70 font-bold tracking-wide">
+          Full Stack Developer
+        </CardContent>
+        <CardFooter className="text-black/60 text-sm justify-between w-full">
+          <div className="flex gap-1 items-center mt-2">
+            <PhoneIcon className="size-4 text-black" /> +91 9912437786{" "}
+          </div>
+          <div className="flex gap-1 items-center mt-2">
+            <EnvelopeIcon className="size-4 text-black" />{" "}
+            ameershaik.cs@gmail.com{" "}
+          </div>
+          <div className="flex gap-1 items-center mt-2">
+            <LocationEdit className="size-4 text-black" /> Kakinada,
+            AndhraPradesh{" "}
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
