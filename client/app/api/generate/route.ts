6deb9a873fbdbc4433762b234c148db3c6161f74
Ameer77-tech <ameershaik.cs@gmail.com>
2026-01-10 13:14:24 +0530

    pdf server route added

--- client/app/api/generate/route.ts
+++ client/app/api/generate/route.ts
@@ -4,7 +4,7 @@ export async function POST(req : Request){
     try{
     const json = await req.json()
     const html = json.html
-    const response = await fetch(`${process.env.PDF_SERVER}/api/generate-pdf`, 
+    const response = await fetch(`${process.env.NEXT_PUBLIC_PDF_SERVER}/api/generate-pdf`, 
        {
           method: "POST",
           headers: {
